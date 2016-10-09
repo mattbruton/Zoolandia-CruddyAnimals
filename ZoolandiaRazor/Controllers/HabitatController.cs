384caa75125d436222ea638548290ea0d249fa5e
MATT BRUTON <the.bruton@gmail.com>
2016-10-08 19:20:59 -0500

    rebuild project, testing db

---
+++ ZoolandiaRazor/Controllers/HabitatController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ZoolandiaRazor.Controllers
+{
+    public class HabitatController : Controller
+    {
+        // GET: Habitat
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
